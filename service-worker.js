f4012f97768e71a9ae25c31222150c7ae65b7507
emmanguyen <emma.mphuong@gmail.com>
2024-08-06 05:04:09 -0700

    Update CSP

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,15 @@ self.addEventListener('fetch', event => {
             });
 
             // Set or update the Content-Security-Policy header
-            newResponse.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-cuBBENZM+PA7y0syXw3+Bw==' 'unsafe-inline'; object-src 'none';");
-
+            newResponse.headers.set(
+                'Content-Security-Policy',
+                "default-src 'self';" +
+                "object-src 'none';" +
+                "base-uri 'self';" +
+                "img-src 'self' data:;" +
+                "script-src 'self' 'nonce-cuBBENZM+PA7y0syXw3+Bw==' 'unsafe-inline';" +
+                "style-src 'self' 'unsafe-inline';" +
+                "frame-src https://securedgenai.substack.com;");
             return newResponse;
         })
     );
